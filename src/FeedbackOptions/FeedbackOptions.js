17a72989f109e4c59b60c56506c5a3c0a25d26e8
17eugene <drivercoua@gmail.com>
2021-09-06 16:52:38 +0300

    added files

--- src/FeedbackOptions/FeedbackOptions.js
+++ src/FeedbackOptions/FeedbackOptions.js
@@ -1,27 +1,28 @@
-import PropTypes from 'prop-types';
-import shortid from 'shortid';
+import PropTypes from "prop-types";
+import shortid from "shortid";
 
-import styles from './FeedbackOptions.module.css'
+import styles from "./FeedbackOptions.module.css";
 
 function FeedbackOptions({ options, onLeaveFeedback }) {
-    return (
-        <div>
-            {options.map(option => (
-                <button
-                    className={styles.feedback__btn}
-                    key={shortid.generate()}
-                    type='button'
-                    onClick={() => onLeaveFeedback(option)}
-                >{option}
-                </button>
-            ))}
-        </div>
-    );
-};
+  return (
+    <div>
+      {options.map((option) => (
+        <button
+          className={styles.feedback__btn}
+          key={shortid.generate()}
+          type="button"
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+}
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string).isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired,
-}
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
 
 export { FeedbackOptions };
